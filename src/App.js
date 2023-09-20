014e93d7d3e43b1b6eeb0bc319cc1d43a6807c24
Pls <139981876+plszz@users.noreply.github.com>
2023-09-20 22:17:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'; // You will create this CSS file for Dark Mode styling
+import modsData from './mods.json';
 
 function App() {
+  // Function to render the list of moderators
+  const renderModsList = () => {
+    return (
+      <ul className="mods-list">
+        {Object.entries(modsData).map(([modName, discordId]) => (
+          <li key={discordId} className="mods-list-item">
+            <div className="mod-pfp">
+              <img src={`https://cdn.discordapp.com/avatars/${discordId}/avatar.png`} alt={`${modName}'s Avatar`} />
+            </div>
+            <div className="mod-name">{modName}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App dark-mode">
+      {renderModsList()}
     </div>
   );
 }
 
 export default App;
+
