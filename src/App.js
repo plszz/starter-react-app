604a1d98c9f06dd34ae9470e0cfd32bfec961471
Pls <139981876+plszz@users.noreply.github.com>
2023-09-21 01:16:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 // App.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import modsData from './mods.json'; // Import the JSON data directly
+import axios from 'axios';
+import modsData from './mods.json'; // love y'all mods!
 
 function App() {
+  const [avatarsData, setAvatarsData] = useState({});
+  const botToken = 'MTE0MDU1MDU3NTIxMDExMDk5OA.GB_gnj.eqyiyKQknoHLN_A-B5W0hysQRSZVXSnCOp6NNI'; 
+
+  useEffect(() => {
+    // Fetch avatars using Axios
+    axios.get('https://discord.com/api/v10/users', {
+      headers: {
+        Authorization: `Bot ${botToken}`,
+      },
+      params: {
+        ids: Object.values(modsData).join(','), // test
+      },
+    })
+      .then(response => {
+        setAvatarsData(response.data); // test
+      })
+      .catch(error => {
+        console.error('Error fetching avatars:', error);
+      });
+  }, [botToken]);
+
   return (
     <div className="container">
       <div className="header">
@@ -14,7 +36,7 @@ function App() {
       <ul className="list">
         {Object.keys(modsData).map(modName => (
           <li className="list-item big" key={modName}>
-            <img className="icon" src={`https://discordapp.com/api/users/${modsData[modName]}/avatars/${modsData[modName]}.png`} alt={`${modName}'s avatar`} />
+            <img className="icon" src={`https://cdn.discordapp.com/avatars/${modsData[modName]}/${avatarsData[modsData[modName]].avatar}.png`} alt={`${modName}'s avatar`} />
             <div className="mod-info">
               <p className="mod-name">{modName}</p>
               <p className="mod-username">(@{modName})</p>
