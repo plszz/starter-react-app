8334b371921d712f1095e9f32652dc2d57404fab
Pls <139981876+plszz@users.noreply.github.com>
2023-09-21 00:36:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 // App.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import ModsList from './ModsList';
+import axios from 'axios';
 
 function App() {
+  const [modsData, setModsData] = useState({});
+
+  useEffect(() => {
+    // Fetch data from mods.json
+    axios.get('mods.json')
+      .then(response => {
+        setModsData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching mods data:', error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <div className="header">
         Kajiwoto Mods
       </div>
-      <ModsList />
+      <h2>Live Room Mods:</h2>
+      <ul className="list">
+        {Object.keys(modsData).map(modName => (
+          <li className="list-item" key={modName}>
+            <img className="icon" src={`https://discordapp.com/api/users/${modsData[modName]}/avatars/${modsData[modName]}.png`} alt={`${modName}'s avatar`} />
+            <a href={`https://kajiwoto.ai/u/@${modName}`}>{modName}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
