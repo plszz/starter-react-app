92200271fa79dd000b2048bb2eb0559a5d7c18a1
Pls <139981876+plszz@users.noreply.github.com>
2023-09-21 00:17:32 +0300

    Update ModsModular.js

--- src/components/ModsModular.js
+++ src/components/ModsModular.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const ModsList = () => {
   const [modsData, setModsData] = useState({});
+  const [selectFile, setFile] = setState("")
 
   useEffect(() => {
     // Fetch data from mods.json
