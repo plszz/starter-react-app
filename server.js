ec4494a55b3d21a7cb5c152a2f8604c01150dae4
Pls <139981876+plszz@users.noreply.github.com>
2023-09-21 01:35:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 
 const express = require('express')
 const path = require("path");
-const app = express()
+const cors = require('cors');
+const app = express();
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
+
+const corsOptions = {
+  origin: 'https://kajiwoto.cyclic.cloud', // Replace with your actual domain
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // for CORS access 
+
+
 var options = {
   dotfiles: 'ignore',
   etag: false,
